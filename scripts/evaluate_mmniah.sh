aad2496cfe84d6dad1ed9a4ddfec6a0463f47c36
NipElement <yuansheng.ni@uwaterloo.ca>
2025-01-16 15:34:30 +0000

    update

--- scripts/evaluate_mmniah.sh
+++ scripts/evaluate_mmniah.sh
@@ -5,13 +5,16 @@ LOG_DIR=eval_logs/mmniah/internvl2_5_8b
 
 mkdir -p $LOG_DIR  # Create log directory
 
+# declare -a tasks=( \
+#     'retrieval-text-test' \
+#     'retrieval-image-test' \
+#     'counting-text-test' \
+#     'counting-image-test' \
+#     'reasoning-text-test' \
+#     'reasoning-image-test' \
+# )
 declare -a tasks=( \
-    'retrieval-text-test' \
-    'retrieval-image-test' \
-    'counting-text-test' \
-    'counting-image-test' \
     'reasoning-text-test' \
-    'reasoning-image-test' \
 )
 
 model_name="internvl2_5"
@@ -20,14 +23,14 @@ model_name="internvl2_5"
 for task in "${tasks[@]}"; do
     echo "$(date) Starting task: ${task}"
 
-    CUDA_VISIBLE_DEVICES=4,5,6,7 \
+    CUDA_VISIBLE_DEVICES=2,3,4,5,6,7 \
     torchrun \
         --nproc_per_node=1 \
         eval/mm_niah/eval_mm_niah.py \
         --checkpoint $CHECKPOINT \
         --outputs-dir $LOG_DIR \
         --task $task \
-        --num-gpus-per-rank=4 \
+        --num-gpus-per-rank=6 \
         > "${LOG_DIR}/${task}.log" 2>&1
     wait
     echo "$(date) Finished task: ${task}"
