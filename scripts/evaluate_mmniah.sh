c711b4400df574c08c7f78b4e25484c43fd75a6a
NipElement <yuansheng.ni@uwaterloo>
2025-01-17 02:33:36 +0000

    update

--- scripts/evaluate_mmniah.sh
+++ scripts/evaluate_mmniah.sh
@@ -5,16 +5,13 @@ LOG_DIR=eval_logs/mmniah/internvl2_5_8b
 
 mkdir -p $LOG_DIR  # Create log directory
 
-# declare -a tasks=( \
-#     'retrieval-text-test' \
-#     'retrieval-image-test' \
-#     'counting-text-test' \
-#     'counting-image-test' \
-#     'reasoning-text-test' \
-#     'reasoning-image-test' \
-# )
 declare -a tasks=( \
-    'reasoning-text-test' \
+    'retrieval-text-test' \ done
+    'retrieval-image-test' \ done
+    'counting-text-test' \
+    'counting-image-test' \
+    'reasoning-text-test' \ done
+    'reasoning-image-test' \ done
 )
 
 model_name="internvl2_5"
@@ -23,14 +20,14 @@ model_name="internvl2_5"
 for task in "${tasks[@]}"; do
     echo "$(date) Starting task: ${task}"
 
-    CUDA_VISIBLE_DEVICES=2,3,4,5,6,7 \
+    CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 \
     torchrun \
         --nproc_per_node=1 \
         eval/mm_niah/eval_mm_niah.py \
         --checkpoint $CHECKPOINT \
         --outputs-dir $LOG_DIR \
         --task $task \
-        --num-gpus-per-rank=6 \
+        --num-gpus-per-rank=8 \
         > "${LOG_DIR}/${task}.log" 2>&1
     wait
     echo "$(date) Finished task: ${task}"
